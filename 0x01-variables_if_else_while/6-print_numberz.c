d9dff08b44ffead1376a823b36ba8db352b970a3
Jedidja Kakuva <jedidja96@outlook.com>
2023-09-16 01:34:53 -0700

    Print numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,14 +9,14 @@
  */
 int main(void)
 {
-    int num = '0';
+	int num = '0';
 
-    while (num <= '9')
-    {
-        putchar(num);
-        num++;
-    }
-    putchar('\n');
-    return (0);
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
 }
 
